fd185dce70aa834739ce952cce22d857187a7bba
hasshe94 <hasshe94@kingshigh.school.nz>
2022-08-03 14:54:14 +1200

    ye
    ye

--- Scenes/SwingingTrap.gd
+++ Scenes/SwingingTrap.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 
-func _on_Area2D_body_enetered(body):
-	if body.is_in_groups("Player"):
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
 		print("Player Dead")
-		
