7a47400667eb5b1152f5d800c51fb1c8246c19e8
hasshe94 <hasshe94@kingshigh.school.nz>
2022-08-10 19:47:59 +1200

    ye
    ye

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -8,3 +8,12 @@ func _process(delta):
 	pass
 	if Input.is_action_just_pressed("reset"):
 		GameStats.reset()
+	print(GameStats.get_time())
+	$CanvasLayer/Control/Label.text = GameStats.get_time()
+
+
+
+
+func _on_Area2D_body_enetered(body):
+	pass
+
