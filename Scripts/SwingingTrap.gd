9ac44dca02a1a9f2c748e4421e9fcf9c844e6722
hasshe94 <hasshe94@kingshigh.school.nz>
2022-08-11 00:41:27 +1200

    ye
    ye

--- Scripts/SwingingTrap.gd
+++ Scripts/SwingingTrap.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+func _ready():
+	$AnimationPlayer.play("Swing")
 
 
+
+
+#manually input the inspector/node thing if code will be used again below
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.is_in_ground("Player"):
 		print("player dead")
-		if GameStats.check_reset() == false:
+		if GameStats.check_reset() ==  false:
 			body.global_position = GameStats.get_spawn().global_position
+
+	
