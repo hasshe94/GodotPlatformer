47fa62045848bd5549059417c24828ed17778610
hasshe94 <hasshe94@kingshigh.school.nz>
2022-08-25 12:25:23 +1200

    e
    e

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -6,24 +6,29 @@ var center = Vector2.ZERO
 
 func _ready():
 	center = get_viewport_rect().size/2
+	target = owner.get_node("Player")
+	
+	position = target.global_position
+	zoom = Vector2(0.3,0.3)
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("zoom"):
 		if zoomed:
-			target = center
+			target = null
 			zoomed = false
 		else:
 			target = owner.get_node("Player").global_position
 			zoomed = true
 			
 	if zoomed:
-		zoom = zoom.move_toward(Vector2(0.3,0.3), 0.03)
-		position = position.move_toward(target,80)
+		position = target.global_position
+		zoom = Vector2(0.3,0.3)
 	
 	else:
-			zoom = zoom.move_toward(Vector2(1,1),0.03)
-			position = position.move_toward(target,80)
+		position = center
+		zoom = Vector2(1,1)
+			
 	
 		
 
